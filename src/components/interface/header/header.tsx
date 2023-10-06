ee5f87f87f9c394a0d722a8a092428d6373ab035
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-06 14:21:16 -0300

    feat: Header component

---
+++ src/components/interface/header/header.tsx
@@ -0,0 +1,22 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+import { Logo } from "./logo";
+import { Navbar } from "./navigation/navbar";
+
+type HeaderProps = ComponentProps<"header">;
+
+export const Header = ({ className, ...props }: HeaderProps) => {
+  return (
+    <header
+      className={twMerge(
+        "bg-main-red w-full h-14",
+        className
+      )}
+      {...props}
+    >
+      <Navbar>
+        <Logo />
+      </Navbar>
+    </header>
+  );
+};
