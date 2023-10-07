19092db0aedcbd375f6a2934e97513a38ab90598
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-07 01:03:23 -0300

    feat: ui items

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,10 @@
+import { ComponentProps } from "react"
+import { twMerge } from "tailwind-merge"
+
+type ButtonProps = ComponentProps<"button">
+
+export const Button = ({className, ...props}: ButtonProps) => {
+  return (
+    <button className={twMerge("w-full h-10 uppercase text-white font-semibold bg-red-main", className)} {...props}/>
+  )
+}
