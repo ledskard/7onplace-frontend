6d7609654b5ea9c2ac4577d7b841e286d95c0e89
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-08 16:36:42 -0300

    feat: componentes defaults dos forms

---
+++ src/components/interface/form-default/form.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+type FormRootProps = ComponentProps<"form">;
+
+export const FormRoot = ({ className, ...props }: FormRootProps) => {
+  return (
+    <form
+      className={twMerge(
+        "flex flex-col gap-6 md:gap-8 sm:max-w-xl justify-center items-center h-[92vh] w-10/12 max-w-5xl m-auto",
+        className
+      )}
+      {...props}
+    />
+  );
+};
