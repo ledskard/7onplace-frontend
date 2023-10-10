a5d985cf676da15717bd38c868df5e071e2aa8bf
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-10 19:49:49 -0300

    feat: schema de registro

---
+++ src/schemas/register-model-schema.ts
@@ -0,0 +1,60 @@
+import z from "zod";
+
+const maxFileSize = 1024 * 1024 * 5; // 5MB
+const acceptedImageTypes = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+export const registerSchema = z.object({
+  name: z.string().min(2, "Campo nome deve conter pelo menos 2 dígitos"),
+  perfilImg: z
+    .any()
+    .refine((files: Array<File>) => {
+      console.log(files);
+      if (files.length === 0) {
+        return false;
+      }
+      return files[0]?.size >= maxFileSize;
+    }, `Tamanho máximo do arquivo é de 5MB.`)
+    .refine((file: Array<File>) => {
+      if (file.length === 0) {
+        return false;
+      }
+      return acceptedImageTypes.includes(file[0]?.type);
+    }, "Somente os formatos .jpg, .jpeg, .png e .webp são suportados"),
+  displayImg: z
+    .any()
+    .refine((files: Array<File>) => {
+      if (files.length === 0) {
+        return false;
+      }
+      return !files?.find((img) => {
+        return img.size >= maxFileSize;
+      });
+    }, `Tamanho máximo do arquivo é de 5MB.`)
+    .refine((files: Array<File>) => {
+      console.log(files);
+      if (files.length === 0) {
+        return false;
+      }
+      return acceptedImageTypes.includes(files[0]?.type);
+    }, "Somente os formatos .jpg, .jpeg, .png e .webp são suportados"),
+  telegramVip: z
+    .string()
+    .regex(
+      /\bhttps?:\/\/t\.me\/[A-Za-z0-9_]+\b/g,
+      "Só aceitamos links do telegram"
+    ),
+  telegramFree: z
+    .string()
+    .regex(
+      /\bhttps?:\/\/t\.me\/[A-Za-z0-9_]+\b/g,
+      "Só aceitamos links do telegram"
+    ),
+  description: z
+    .string()
+    .min(10, "Descrição deve conter pelo menos 10 caracteres"),
+});
