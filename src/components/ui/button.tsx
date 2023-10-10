3465d9d0b41b537bc82c90c8b0a08377ab8ff0eb
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-10 19:48:56 -0300

    feat: componente button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -7,7 +7,7 @@ export const Button = ({ className, ...props }: ButtonProps) => {
   return (
     <button
       className={twMerge(
-        "w-full h-10 uppercase text-white font-semibold bg-red-main",
+        "w-full h-10 uppercase text-white font-semibold bg-red-main rounded md:rounded-md",
         className
       )}
       {...props}
