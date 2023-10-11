f989b8dcf8175fa28c927a0c81bd8410dae0386b
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-11 12:34:31 -0300

    feat: verificação de rota privada

--- src/app/admregister/page.tsx
+++ src/app/admregister/page.tsx
@@ -1,5 +1,10 @@
+import { getServerSession } from "next-auth";
 import { FormRegisterContainer } from "./form-register-container";
+import { redirect, useRouter } from "next/navigation";
 
-export default function AdmRegister() {
+export default async function AdmRegister() {
+  const session = await getServerSession();
+  if (!session) redirect("/admlogin");
+
   return <FormRegisterContainer />;
 }
