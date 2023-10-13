e60168a7b08dc311e213595bc1bc9b200d55372e
‘YuriLRodrigues’ <rodriguesyuri769@gmail.com>
2023-10-12 23:58:47 -0300

    fix: fixando bugs de acessar sem permissao

--- src/app/admlogin/page.tsx
+++ src/app/admlogin/page.tsx
@@ -1,6 +1,9 @@
+import { getServerSession } from "next-auth";
 import { FormLoginContainer } from "./components/form-login-container";
+import { redirect } from "next/navigation";
 
-export default function AdmLogin() {
+export default async function AdmLogin() {
+  const session = await getServerSession();
+  if (session) return redirect("/admregister");
   return <FormLoginContainer />;
 }
-
--- src/app/admregister/page.tsx
+++ src/app/admregister/page.tsx
@@ -1,10 +1,14 @@
 import { getServerSession } from "next-auth";
 import { FormRegisterContainer } from "./form-register-container";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function AdmRegister() {
-  // const session = await getServerSession();
-  // if (!session) redirect("/admlogin");
+  const session = await getServerSession();
+  if (!session) redirect("/admlogin");
 
-  return <FormRegisterContainer />;
+  return (
+    <>
+      <FormRegisterContainer />
+    </>
+  );
 }
