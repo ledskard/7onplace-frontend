548da77ad9fb9f3c679b92dc205ec209cb0cd2ad
cunha <luiiz.cunha@hotmail.com>
2023-10-16 23:43:38 -0300

    test

--- src/types/model/get-models-by-filter.ts
+++ src/types/model/get-models-by-filter.ts
@@ -4,6 +4,7 @@ export const getModelsByFilter = async (modelType: string) => {
       `https://alb7onsexy-1208292946.us-east-1.elb.amazonaws.com/models/?type=${modelType}`,
       { next: { revalidate: 1 } }
     );
+    console.log(modelsByFilter)
     const modelsjson = await modelsByFilter.json();
     return modelsjson;
   } catch (error) {
