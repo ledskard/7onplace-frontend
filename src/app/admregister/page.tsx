46c41de2d768e38d29c3c347db4042490eec38a5
cunha <luiiz.cunha@hotmail.com>
2023-10-17 20:42:31 -0300

    a

--- src/app/admregister/page.tsx
+++ src/app/admregister/page.tsx
@@ -3,8 +3,8 @@ import { FormRegisterContainer } from "./form-register-container";
 import { redirect } from "next/navigation";
 
 export default async function AdmRegister() {
-  const session = await getServerSession();
-  if (!session) redirect("/admlogin");
+  // const session = await getServerSession();
+  // if (!session) redirect("/admlogin");
 
   return (
     <>
