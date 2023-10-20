9a984bbcd218932cc56591986e906ccfbb4bd6c9
cunha <luiiz.cunha@hotmail.com>
2023-10-20 13:36:29 -0300

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "@/providers/providers";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: "7OnSexy",
@@ -21,6 +22,7 @@ export default function RootLayout({
           <Header />
           <Toaster />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
