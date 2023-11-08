de75bcdd0901482be2e5ce5461cb0d46fe9d1127
cunha <luiiz.cunha@hotmail.com>
2023-11-08 10:37:28 -0300

    a

--- src/types/model/get-models-by-filter.ts
+++ src/types/model/get-models-by-filter.ts
@@ -2,7 +2,7 @@ export const getModelsByFilter = async (modelType: string) => {
   try {
     const modelsByFilter = await fetch(
       `${process.env.NEXT_PUBLIC_DATABASE_URL}/models/?type=${modelType}`,
-      { next: { revalidate: 1 } }
+      { next: { revalidate: 6200 } }
     );
     const modelsjson = await modelsByFilter.json();
     return modelsjson;
--- src/utils/get-model-by-id.ts
+++ src/utils/get-model-by-id.ts
@@ -1,7 +1,7 @@
 export const getDataById = async (slug: string) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_DATABASE_URL}/models/${slug}`,
-    { next: { revalidate: 1 } }
+    { next: { revalidate: 6200 } }
   );
   return await res.json();
 };
