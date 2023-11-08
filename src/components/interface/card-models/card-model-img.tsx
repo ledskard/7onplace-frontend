b5b13f990f3d2add7b25c7ad9563e21f939a9872
cunha <luiiz.cunha@hotmail.com>
2023-11-08 15:14:42 -0300

    a

--- src/components/interface/card-models/card-model-img.tsx
+++ src/components/interface/card-models/card-model-img.tsx
@@ -13,7 +13,7 @@ export const CardModelImage = ({
     <Image
       className={twMerge("object-fill object-center", className)}
       fill
-      loading={'lazy'}
+      loading={"lazy"}
       blurDataURL={imgLoading}
       placeholder="blur"
       sizes="(max-width: 1280px) 50vw, 33vw"
