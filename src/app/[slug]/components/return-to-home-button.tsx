f003a326501c6435537d3007ad509b8f522e5800
Luiz <luiiz.cunha@hotmail.com>
2023-12-05 11:56:14 -0300

    a

--- src/app/[slug]/components/return-to-home-button.tsx
+++ src/app/[slug]/components/return-to-home-button.tsx
@@ -11,13 +11,21 @@ export const ReturnToHomeButton = ({
   ...props
 }: ReturnToHomeButtonProps) => {
   const route = useRouter();
+  const handleClick = () => {
+    console.log(window.location.origin)
+    if (document.referrer.startsWith(window.location.origin)) {
+      route.back();
+    } else {
+      route.push('/');
+    }
+  };
   return (
     <button
       className={twMerge(
         "sm:p-1 flex gap-4 text-white items-center my-auto md:h-auto h-full",
         className
       )}
-      onClick={() => route.push('/')}
+      onClick={handleClick}
       {...props}
 
     >
