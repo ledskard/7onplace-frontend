d093703e601818ed688d86835d5a84a161287d00
Luiz <luiiz.cunha@hotmail.com>
2023-12-05 16:09:07 -0300

    a

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+    // console.log("AQUIIUAIIII", request.headers.get('referer'))
+
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/', '/MaikellyMuhl'],
+}
