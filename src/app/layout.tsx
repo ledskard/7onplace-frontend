f071d590daa5553c8e3684c31bb98871694a3138
Luiz <luiiz.cunha@hotmail.com>
2023-12-06 13:55:05 -0300

    remove analytcs change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Analytics } from '@vercel/analytics/react';
 export const metadata: Metadata = {
   title: "7 On Place",
   description: "Maiores criadoras(es) de conteúdo hot em um só lugar!",
-  openGraph: { images: 'https://scontent-gru1-1.xx.fbcdn.net/v/t39.30808-1/340768899_529830022563190_3934893358219137785_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=596444&_nc_ohc=58HoXk8U-tcAX_L6Zho&_nc_ht=scontent-gru1-1.xx&oh=00_AfBzFe2DVt0h-QNMjKZ7K8--YMIreCzRTINLwB6x7hHQwg&oe=656D0C80' },
+  openGraph: {images: 'https://scontent-gru1-1.xx.fbcdn.net/v/t39.30808-1/340768899_529830022563190_3934893358219137785_n.jpg?stp=dst-jpg_p200x200&_nc_cat=110&ccb=1-7&_nc_sid=596444&_nc_ohc=58HoXk8U-tcAX_L6Zho&_nc_ht=scontent-gru1-1.xx&oh=00_AfBzFe2DVt0h-QNMjKZ7K8--YMIreCzRTINLwB6x7hHQwg&oe=656D0C80'},  
 };
 
 export default function RootLayout({
@@ -25,17 +25,10 @@ export default function RootLayout({
             <Toaster />
             <main className="max-w-[1930px] w-full mx-auto flex-1 flex flex-col my-4">
               {children}
-              <Analytics
-                beforeSend={(event) => {
-                  if (event.url.localeCompare('/')) {
-                    return null;
-                  }
-                  return event;
-                }}
-              />
+              <Analytics />
             </main>
           </div>
-
+          
 
         </Providers>
       </body>
