17680b7c0103af68b5eb32f9093e40db8c795b38
Luiz <luiiz.cunha@hotmail.com>
2024-01-09 18:50:23 -0300

    a

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -56,6 +56,7 @@ export default async function Model({ params }: { params: { slug: string } }) {
                 but.title !== "" &&
                 but.url !== ""
               )
+              
                 return (
                   <ButtonSocialMedia
                     key={but.id}
