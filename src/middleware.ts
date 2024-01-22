f7abfe72111871085bbda27c0a2c2dbd9151d2f3
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-01-22 15:15:29 -0300

    feat: validation token exp

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,10 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { cookies } from "next/headers"
+import { getServerSession } from 'next-auth';
+import { getSession } from 'next-auth/react';
+import { getToken } from 'next-auth/jwt';
+import jwt from 'jsonwebtoken'
 
 let pathname;
 
@@ -12,6 +16,30 @@ export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
     referrer !== null ? 
     response.cookies.set('referrer', '7onplace') : response.cookies.set('referrer', 'another_site')
+  
+    const session = await getToken({
+      req: request,
+    });
+    console.log('session: ', session)
+
+    if (session?.token) {
+      const tokenPayload = jwt.decode(session?.token) as {
+        exp: number
+      }
+
+      if (typeof tokenPayload === "string" || !tokenPayload) return session
+
+      const tokenIsExpired = new Date() < new Date(tokenPayload.exp)
+
+      if (!tokenIsExpired) {
+        console.log('removendo cookies - deslogou')
+
+        response.cookies.delete('next-auth.session-token')
+        response.cookies.delete('next-auth.callback-url')
+        response.cookies.delete('next-auth.csrf-token')
+      }
+    }
+
     return response
   }
 
