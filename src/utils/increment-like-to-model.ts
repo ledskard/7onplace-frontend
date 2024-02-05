81240607cee4e9dfc68b0566ceb376700c960b3e
Luiz <luiiz.cunha@hotmail.com>
2024-02-05 11:11:02 -0300

    a

--- src/utils/increment-like-to-model.ts
+++ src/utils/increment-like-to-model.ts
@@ -2,6 +2,9 @@ import revalidateTagAPI from "@/actions/revalidateTag";
 
 export const incrementLike = async (slug: string) => {
   try {
+    if(slug.toLowerCase() === "MorenaMineira".toLowerCase() || slug.toLowerCase() === "PollyannaPersch".toLowerCase()){
+      return
+    }
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_DATABASE_URL}/models/${slug}`,
       {
