a00a0e3d82c3f2fe4c5ec343953a4e02059846a3
YuriLRodrigues <rodriguesyuri769@gmail.com>
2024-03-28 10:33:58 -0300

    feat: filter params

--- src/components/interface/search/search-input.tsx
+++ src/components/interface/search/search-input.tsx
@@ -1,46 +1,30 @@
 "use client";
 
-import { Search } from "lucide-react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { ComponentProps, useCallback, useEffect, useRef } from "react";
+import { ComponentProps, useEffect, useRef } from "react";
+
+import { QueryParams } from "@/utils/query-params";
+import { Search } from "lucide-react";
 import { twMerge } from "tailwind-merge";
 
 type SearchInputProps = ComponentProps<"div">;
 
-export const SearchInput = ({ className, ...props }: SearchInputProps) => {
+export const SearchInput = ({ className }: SearchInputProps) => {
   const searchRef = useRef<HTMLInputElement>(null);
 
   const router = useRouter();
   const pathname = usePathname();
+
   const searchParams = useSearchParams();
-  const defaultValue = searchParams.get('query')
+  const tab = searchParams.get("tab");
 
-  const createQueryString = useCallback(
-    (name: string, value: string) => {
-      const params = new URLSearchParams(searchParams as unknown as string);
-      params.set(name, value);
-      params.delete("page");
-
-      return params.toString();
-    },
-    [searchParams]
-  );
-
-  const removeQueryString = useCallback(
-    (name: string) => {
-      const params = new URLSearchParams(searchParams as unknown as string);
-      params.delete(name);
-
-      return params.toString();
-    },
-    [searchParams]
-  );
+  const query = searchParams.get("query");
 
   // Teclas de atalho ESC para limpar o campo de busca
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
-        router.push(pathname + "?" + removeQueryString("query"));
+        router.push("/");
       }
     };
 
@@ -49,15 +33,26 @@ export const SearchInput = ({ className, ...props }: SearchInputProps) => {
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, [router, pathname, removeQueryString]);
+  }, [router, pathname]);
 
   const handleSearchModels = () => {
     const value = searchRef.current?.value.toLowerCase();
 
     if (value) {
-      router.push(pathname + "?" + createQueryString("query", value));
+      const url = QueryParams.baseUrl(pathname)
+        .query({
+          query: "tab",
+          value: tab ?? "mulheres",
+        })
+        .query({
+          query: "query",
+          value: value ?? "",
+        })
+        .value();
+
+      router.push(url);
     } else {
-      router.push(pathname + "?" + removeQueryString("query"));
+      router.push("/");
     }
   };
 
@@ -65,7 +60,7 @@ export const SearchInput = ({ className, ...props }: SearchInputProps) => {
     <div
       className={twMerge(
         "sm:flex hidden md:ml-4 items-center justify-between border-2 border-red-main rounded md:rounded-md p-1 max-w-[70%]",
-        className
+        className,
       )}
     >
       <input
@@ -73,10 +68,10 @@ export const SearchInput = ({ className, ...props }: SearchInputProps) => {
         type="text"
         className="bg-transparent outline-none max-w-[80%]"
         placeholder="Buscar modelos"
-        defaultValue={defaultValue || undefined}
+        defaultValue={query || undefined}
         onKeyUp={handleSearchModels}
       />
-      <button onClick={handleSearchModels} aria-label='Buscar modelos'>
+      <button onClick={handleSearchModels} aria-label="Buscar modelos">
         <Search className="w-10/12" />
       </button>
     </div>
