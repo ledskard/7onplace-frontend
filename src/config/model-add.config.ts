9e32918ce33e574968e5b72f99418edbdea09f7b
Luiz Cunha <49279873+ledskard@users.noreply.github.com>
2024-05-14 15:12:26 -0300

    a

--- src/config/model-add.config.ts
+++ src/config/model-add.config.ts
@@ -1,30 +1,25 @@
 type ModelAddsProps = {
-  image: string,
-  href: string
-  alt: string
-}
+  image: string;
+  href: string;
+  alt: string;
+};
 
 type ModelAddsConfigProps = {
-  default: ModelAddsProps
-  ads: Array<ModelAddsProps>
-}
-
+  default: ModelAddsProps;
+  ads: Array<ModelAddsProps>;
+};
 
 export const modelAddsConfig: ModelAddsConfigProps = {
   default: {
-    image: '/anuncio-7onplace.png',
-    href: 'https://wa.me//48991013165?text=Gostaria%20de%20anunciar%20no%20marketplace%20da%207%20On%20Sexy',
-    alt: 'image default',
+    image: "/anuncio-7onplace.png",
+    href: "https://wa.me//48991013165?text=Gostaria%20de%20anunciar%20no%20marketplace%20da%207%20On%20Sexy",
+    alt: "image default",
   },
   ads: [
     {
-      image: '/template-ads.png',
-<<<<<<< HEAD
-      href: 'https://t.me/addlist/In7adwzKX1U4MzUx',
-=======
-      href: 'https://t.me/addlist/rKDJy03gt4szYzMx',
->>>>>>> 8d0ced849782c66f802c63cbe6d238bfa2288df4
-      alt: 'ad 7onsexy',
-    }
-  ]
-}
+      image: "/template-ads.png",
+      href: "https://t.me/addlist/rKDJy03gt4szYzMx",
+      alt: "ad 7onsexy",
+    },
+  ],
+};
