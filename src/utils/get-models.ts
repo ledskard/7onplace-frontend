294176554272a3a2478007c912e8f7ac40bfa55d
luiz cunha <luizcunha2904@gmail.com>
2024-09-17 13:37:23 -0300

    fix

--- src/utils/get-models.ts
+++ src/utils/get-models.ts
@@ -19,7 +19,7 @@ export const getModels = async ({
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_DATABASE_URL}/models?page=${page}&type=${type}&filter=${query}`,
       {
-        next: { revalidate: 3600, tags: ["modelsList"] },
+        next: { revalidate: 259200 },
         headers: {
           "Content-Type": "application/json",
           Authorization: "Bearer " + session?.user.token,
